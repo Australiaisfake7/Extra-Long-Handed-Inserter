282d8c763107188b1de3f3f026321eb6f3efbf6d
Australiaisfake7 <michaeloverend900@gmail.com>
2026-01-29 09:41:52 +1300

    Delete extra-long-handed-inserter_0.3.0 directory
    Replaced with versioning

--- extra-long-handed-inserter_0.3.0/data-updates.lua
+++
@@ -1,8 +0,0 @@
-if data.raw.technology["automation-2"] then
-	table.insert(data.raw.technology["automation-2"].effects,
-		{
-			type = "unlock-recipe",
-			recipe = "extra-long-handed-inserter"
-		}
-	)
-end
